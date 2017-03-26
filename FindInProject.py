3044d2769ab2837a5cbb20c150439b9523d45d64
Wramberg <jesper.wramberg@gmail.com>
2017-03-26 14:33:10 +0200

    Initial commit
    Initial commit with all basic functionality in place

---
+++ FindInProject.py
@@ -0,0 +1,179 @@
+import time
+import queue
+import threading
+
+import sublime_plugin
+import sublime
+
+from . import filesearcher
+from . import resultbuffer
+
+## Remember
+# incase sensitive - done
+# status bar in top - no needed
+# mark result in buffer - done
+# copy line without getting the line number etc - not gonna do atm
+# search all project dirs and not just the first of them - done
+# skip large files - done
+# follow symlinks - done
+# attempt to detect binaries - done
+# concatenate lines if too long - done - remember the bad handling of multiple hits tho
+# make max line size configurable - done
+# project speicifc settings - should be doable through ST ?!?!
+# cancel search thread - done
+# fix num hits - done
+# fix result buffer folding - done
+# setting for size and encoding notifications - done
+# fix mouse + keyboard weird bug - done
+
+# to explain what happens with multiple hits on one line
+# cleanup this file and result buffer
+
+class FindInProject(sublime_plugin.TextCommand):
+   """
+   Find In Project - a Sublime Text 3 plugin for text search in projects. The
+   search is performed in a background thread and results are presented in an
+   interactive view.
+   """
+   def __init__(self, view):
+      sublime_plugin.TextCommand.__init__(self, view)
+      self.result_queue = None
+      self.result_buffer = None,
+      self.search_thread = None
+      self.status_string_idx = 0;
+      self.num_hits = 0
+      self.num_file_hits = 0
+      self.last_status_update = 0
+      self.search_cancelled = False
+
+      settings = sublime.load_settings('FindInProject.sublime-settings')
+      self.excessive_hits_count = settings.get('find_in_project_excessive_hits_count', 5000)
+
+   def run(self, edit):
+      """
+      Show input panel to get target string for search
+      """
+      win = sublime.active_window()
+      view = win.active_view()
+
+      # See if we can find a selection to use as initial guess for target string
+      initial_input_text = ""
+      for region in view.sel():
+            if not region.empty():
+                # Get selected text
+                txt = view.substr(region)
+                if "\n" not in txt:
+                  initial_input_text = txt
+                  break
+
+      # Present user input panel
+      win.show_input_panel("Find in project:", initial_input_text, self.input_panel_on_done, None, None)
+
+   def input_panel_on_done(self, user_input):
+      """
+      Callback for input panel on done. Search files in project for the provided
+      string.
+      """
+      if len(user_input) == 0:
+         return
+
+      # Get active window and view
+      win = sublime.active_window()
+      view = win.active_view()
+
+      # Get project data
+      project_data = win.project_data()
+      if ("folders" not in project_data) or len(project_data["folders"]) == 0:
+         return
+
+      # Reset member vars for new search
+      self.result_queue = queue.Queue()
+      self.status_string_idx = 0;
+      self.num_hits = 0
+      self.num_file_hits = 0
+      self.last_status_update = 0
+      self.search_cancelled = False
+
+      # Search all project dirs
+      project_dirs = []
+      for folder in project_data["folders"]:
+         project_dirs.append(folder["path"])
+
+      # Initialize result buffer/view
+      self.result_buffer = resultbuffer.ResultBuffer(win, user_input)
+
+      # Start search thread
+      self.search_thread = filesearcher.FileSearcherThread(project_dirs, user_input, self.result_queue)
+      self.search_thread.start()
+      print("M Started " + str(time.time()))
+
+      # Come back in 1 ms to start handling results on an alternate thread
+      sublime.set_timeout_async(self.handle_search_results, 1)
+
+   def handle_search_results(self):
+      """
+      Handle search results that search thread places on the result queue
+      """
+      while True:
+         self.update_status()
+
+         # Check if result buffer has been closed - in this case we cancel the
+         # search.
+         if self.search_cancelled == False and self.result_buffer.is_closed():
+            self.search_thread.stop()
+            self.search_cancelled = True
+
+         # If there are any results
+         if not self.result_queue.empty():
+            result = self.result_queue.get()
+
+            # Update number of hits but ensure it does not include warning/error
+            # strings
+            if len(result["result"]):
+               if 0 not in result["result"]:
+                  self.num_hits = self.num_hits + len(result["result"])
+                  self.num_file_hits = self.num_file_hits + 1
+
+            # If we reach excessive limit start dropping results and stop search
+            if (self.excessive_hits_count != 0) and (self.num_hits > self.excessive_hits_count):
+               self.search_thread.stop()
+               self.search_cancelled = True
+
+            # Update result view
+            if not self.search_cancelled:
+               self.result_buffer.insert_result(result)
+            self.result_queue.task_done()
+
+         # Are we done ?
+         if (self.search_thread.isAlive() == False) and (self.result_queue.empty() == True):
+            self.set_final_status()
+            break
+
+   def update_status(self):
+      """
+      Update text in status bar
+      """
+      if time.time() > (self.last_status_update + 0.2):
+         self.last_status_update = time.time()
+         win = sublime.active_window()
+         self.status_string_idx = self.status_string_idx + 1
+
+         if self.status_string_idx == 0:
+            win.status_message("FindInProject: Searching in project | [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
+         elif self.status_string_idx == 1:
+            win.status_message("FindInProject: Searching in project / [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
+         elif self.status_string_idx == 2:
+            win.status_message("FindInProject: Searching in project - [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
+         elif self.status_string_idx > 2:
+            win.status_message("FindInProject: Searching in project \ [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
+            self.status_string_idx = 0
+
+   def set_final_status(self):
+      """
+      Set final text in status bar
+      """
+      win = sublime.active_window()
+      if self.search_cancelled:
+         win.status_message("FindInProject: Search cancelled (due to close or excessive number of hits)")
+      else:
+         win.status_message("FindInProject: Search done [%i hits across %i files]" % (self.num_hits, self.num_file_hits))
---
+++ resultbuffer.py
@@ -0,0 +1,271 @@
+import time
+
+import sublime
+import sublime_plugin
+
+
+class ResultBuffer:
+   """
+   A result buffer for search results.
+   """
+   def __init__(self, win, target_string):
+      self.win = win
+      self.target_string = target_string
+
+      # Get new view
+      view = self.win.new_file()
+
+      # Configure view
+      view.set_name("Found in project")
+      view.set_scratch(True)
+      self.win.focus_view(view)
+
+      view.settings().set('line_numbers', False)
+      view.settings().set("auto_complete_commit_on_tab", False)
+      view.settings().set("fade_fold_buttons", True)
+      view.settings().set("gutter", True)
+      view.settings().add_on_change('color_scheme', lambda: set_proper_scheme(view))
+      view.set_syntax_file('Packages/FindInProject/FindInProject.sublime-syntax')
+
+      # Save view for later
+      self.view = view
+
+   def insert_result(self, result):
+      """
+      Insert results for a file into the buffer.
+      """
+      # Add file name to start of result block
+      result_str = ("\n" + result["filepath"] + "\n")
+
+      # For each result in file add an indented line
+      for line in result["result"].keys():
+         result_str += (str(line).rjust(6) + ": " + result["result"][line])
+         if result_str[-1] != "\n":
+            result_str += "\n"
+
+      self.view.run_command("find_in_project_insert_text",
+                            {"args": {'text': result_str, 'target_string': self.target_string}})
+
+   def is_closed(self):
+      """
+      Check if the result buffer was closed by the user.
+      """
+      open_views = self.win.views()
+      for view in open_views:
+         if view.id() == self.view.id():
+            return False
+      return True
+
+
+def set_proper_scheme(view):
+   """
+   Callback for setting color scheme
+   """
+   if view.settings().get('color_scheme') != "Packages/FindInProject/FindInProject.hidden-tmTheme":
+      view.settings().set('color_scheme', "Packages/FindInProject/FindInProject.hidden-tmTheme")
+
+
+class FindInProjectCommand:
+   """
+   Utility functions for find in project commands. Intended to be used with
+   sublime_plugin.TextCommand classes.
+   """
+   def selection_is_empty_line(self):
+      """
+      Check if selection is an empty line
+      """
+      scope = self.view.scope_name(self.view.sel()[0].begin())
+      if "findinproject.emptyline" in scope:
+         return True
+      return False
+
+   def get_point_at_start_of_selection(self):
+      """
+      Get the point at the start of the line
+      """
+      (row, col) = self.view.rowcol(self.view.sel()[0].begin())
+      return self.view.text_point(row, 0)
+
+   def point_is_file(self, point):
+      """
+      Check if provided point is on a filename
+      """
+      scope = self.view.scope_name(point)
+      if "findinproject.filename" in scope:
+         return True
+      return False
+
+   def point_is_empty_line(self, point):
+      """
+      Check if provided point is on an empty line
+      """
+      scope = self.view.scope_name(point)
+      if "findinproject.emptyline" in scope:
+         return True
+      return False
+
+
+class FindInProjectInsertText(FindInProjectCommand, sublime_plugin.TextCommand):
+   """
+   Insert a blob of text in the view and add regions for all occurences of the
+   target string using the 'findinproject.targetstring' scope.
+   """
+   def __init__(self, args):
+      super().__init__(args)
+      self.region_key_postfix_counter = 1
+
+   def run(self, edit, args):
+      start_view_size = self.view.size()
+      moveCursor = False
+      if start_view_size == 0:
+         moveCursor = True
+
+      self.view.set_read_only(False)
+      self.view.insert(edit, start_view_size, args['text'])
+      self.view.set_read_only(True)
+
+      if moveCursor:
+         self.view.run_command("goto_line", {"line": 2})
+
+      self._highlight_target_string(args['target_string'], start_view_size)
+
+   def _highlight_target_string(self, target_string, start_point):
+      """
+      High target string
+      """
+      # Start by going down two rows so we do not highlight target strings
+      # in filenames
+      (row, col) = self.view.rowcol(start_point)
+      start_point = self.view.text_point(row+2, 0)
+
+      target_regions = []
+      while True:
+         target_region = self.view.find(target_string, start_point, sublime.LITERAL | sublime.IGNORECASE)
+         if target_region is None or target_region.empty():
+            break
+
+         target_regions.append(target_region)
+         start_point = target_region.end()
+
+      key = "FindInProjectHighlight%i" % (self.region_key_postfix_counter, )
+      self.region_key_postfix_counter = self.region_key_postfix_counter + 1
+      # flags = sublime.DRAW_NO_OUTLINE
+      flags = sublime.DRAW_NO_FILL
+      self.view.add_regions(key, target_regions, "findinproject.targetstring", flags=flags)
+
+
+class FindInProjectNextLine(FindInProjectCommand, sublime_plugin.TextCommand):
+   """
+   Go up or down a line in the result view but skip empty lines.
+   """
+   def run(self, edit, forward=True):
+      self.view.run_command("move", {"by": "lines", "forward": forward})
+      if self.selection_is_empty_line():
+         self.view.run_command("move", {"by": "lines", "forward": forward})
+
+      # Always set cursor to col 0 to avoid highlighting issues with folding
+      (row, col) = self.view.rowcol(self.view.sel()[0].begin())
+      target = self.view.text_point(row, 0)
+      reg = sublime.Region(target)
+      self.view.sel().clear()
+      self.view.sel().add(reg)
+      self.view.show(target)
+
+
+class FindInProjectNextFile(FindInProjectCommand, sublime_plugin.TextCommand):
+   """
+   Go up/down to next/previous file.
+   """
+   def run(self, edit, forward=True):
+      # Move a step in requested direction
+      self.view.run_command("move", {"by": "lines", "forward": forward})
+      point = self.get_point_at_start_of_selection()
+
+      while self.point_is_file(point) == False:
+         prev_point = point
+         self.view.run_command("move", {"by": "lines", "forward": forward})
+         point = self.get_point_at_start_of_selection()
+         if point == prev_point:
+            # We are stuck at BOF or EOF
+            return
+
+
+class FindInProjectOpenResult(FindInProjectCommand, sublime_plugin.TextCommand):
+   """
+   Open search result marked by the cursor.
+   """
+   def run(self, edit):
+      win = sublime.active_window()
+      (row, col) = self.view.rowcol(self.view.sel()[0].begin())
+
+      # If cursor is standing on a filename line
+      point = self.view.text_point(row, 0)
+      if self.point_is_file(point):
+         fileline = self.view.line(point)
+         filename = self.view.substr(fileline)
+         win.open_file(filename)
+         return
+
+      # Go upwards and find filename
+      while row is not 0:
+         filepoint = self.view.text_point(row, 0)
+         if self.point_is_file(filepoint):
+            break
+         row = row - 1
+
+      if row is 0:
+         return
+
+      filename = self.view.substr(self.view.line(filepoint))
+      linecontent = self.view.substr(self.view.line(point))
+      lineNo = linecontent.split(':')[0]
+      lineNo = lineNo.strip()
+
+      win.open_file(filename+":"+lineNo, sublime.ENCODED_POSITION)
+
+
+class FindInProjectFold(FindInProjectCommand, sublime_plugin.TextCommand):
+   """
+   Fold/Unfold results for currently selected file.
+   """
+   def run(self, edit, fold=True):
+      # Get active line
+      (row, col) = self.view.rowcol(self.view.sel()[0].begin())
+      if row < 1:
+         return
+
+      # Go upwards and find filename
+      point = self.view.text_point(row, 0)
+      while self.point_is_file(point) == False:
+         row = row - 1
+         if row is 0:
+            # Could not find filename - buffer must be corrupt or something
+            return
+         point = self.view.text_point(row, 0)
+
+      file_row = row
+      file_line_region = self.view.line(point)
+      file_line_end = file_line_region.end()
+
+      # Go downwards and find end of file results
+      while self.point_is_empty_line(point) == False:
+         prev_point = point
+         row = row + 1
+         point = self.view.text_point(row, 0)
+         if point == prev_point:
+            # We reached EOF - fold until here
+            break
+
+      # Get region starting from the end of the filename line and ending at
+      # the end of the last result line for that file
+      folding_region = sublime.Region(file_line_end, point-1)
+      if fold:
+         self.view.fold(folding_region)
+      else:
+         self.view.unfold(folding_region)
+
+      # Move selection to filename if we folded
+      if fold:
+         reg = sublime.Region(self.view.text_point(file_row, 0))
+         self.view.sel().clear()
+         self.view.sel().add(reg)
