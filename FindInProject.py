9f5c096aefb614ab13c1c0819bca673212f367f9
Wramberg <jesper.wramberg@gmail.com>
2017-04-08 19:42:30 +0200

    Change indetation to multiples of 4 spaces and refactor a bit

--- FindInProject.py
+++ FindInProject.py
@@ -10,146 +10,147 @@ from . import resultbuffer
 
 
 class FindInProject(sublime_plugin.TextCommand):
-   """
-   Find In Project - a Sublime Text 3 plugin for text search in projects. The
-   search is performed in a background thread and results are presented in an
-   interactive view.
-   """
-   def __init__(self, view):
-      sublime_plugin.TextCommand.__init__(self, view)
-      self.result_queue = None
-      self.result_buffer = None,
-      self.search_thread = None
-      self.status_string_idx = 0;
-      self.num_hits = 0
-      self.num_file_hits = 0
-      self.last_status_update = 0
-      self.search_cancelled = False
+    """
+    Find In Project - a Sublime Text 3 plugin for text search in projects. The
+    search is performed in a background thread and results are presented in an
+    interactive view.
+    """
+    def __init__(self, view):
+        sublime_plugin.TextCommand.__init__(self, view)
+        self.result_queue = None
+        self.result_buffer = None
+        self.search_thread = None
+        self.status_string_idx = 0
+        self.num_hits = 0
+        self.num_file_hits = 0
+        self.last_status_update = 0
+        self.search_cancelled = False
 
-      settings = sublime.load_settings('FindInProject.sublime-settings')
-      self.excessive_hits_count = settings.get('find_in_project_excessive_hits_count', 5000)
+        settings = sublime.load_settings('FindInProject.sublime-settings')
+        self.excessive_hits_count = settings.get('find_in_project_excessive_hits_count', 5000)
 
-   def run(self, edit):
-      """
-      Show input panel to get target string for search
-      """
-      win = sublime.active_window()
-      view = win.active_view()
+    def run(self, edit):
+        """
+        Show input panel to get target string for search
+        """
+        win = sublime.active_window()
+        view = win.active_view()
 
-      # See if we can find a selection to use as initial guess for target string
-      initial_input_text = ""
-      for region in view.sel():
-         if not region.empty():
-            # Get selected text
-            txt = view.substr(region)
-            if "\n" not in txt:
-               initial_input_text = txt
-               break
+        # See if we can find a selection to use as initial guess for target string
+        initial_input_text = ""
+        for region in view.sel():
+            if not region.empty():
+                # Get selected text
+                txt = view.substr(region)
+                if "\n" not in txt:
+                    initial_input_text = txt
+                    break
 
-      # Present user input panel
-      win.show_input_panel("Find in project:", initial_input_text, self.input_panel_on_done, None, None)
+        # Present user input panel
+        win.show_input_panel("Find in project:", initial_input_text, self.input_panel_on_done, None, None)
 
-   def input_panel_on_done(self, user_input):
-      """
-      Callback for input panel on done. Search files in project for the provided
-      string.
-      """
-      if len(user_input) == 0:
-         return
+    def input_panel_on_done(self, user_input):
+        """
+        Callback for input panel on done. Search files in project for the provided
+        string.
+        """
+        if len(user_input) == 0:
+            return
 
-      # Get active window and view
-      win = sublime.active_window()
-      view = win.active_view()
+        # Get active window and view
+        win = sublime.active_window()
+        view = win.active_view()
 
-      # Get project data
-      project_data = win.project_data()
-      if ("folders" not in project_data) or len(project_data["folders"]) == 0:
-         return
+        # Get project data
+        project_data = win.project_data()
+        if ("folders" not in project_data) or len(project_data["folders"]) == 0:
+            return
 
-      # Reset member vars for new search
-      self.result_queue = queue.Queue()
-      self.status_string_idx = 0;
-      self.num_hits = 0
-      self.num_file_hits = 0
-      self.last_status_update = 0
-      self.search_cancelled = False
+        # Reset member vars for new search
+        self.result_queue = queue.Queue()
+        self.status_string_idx = 0;
+        self.num_hits = 0
+        self.num_file_hits = 0
+        self.last_status_update = 0
+        self.search_cancelled = False
 
-      # Search all project dirs
-      project_dirs = []
-      for folder in project_data["folders"]:
-         project_dirs.append(folder["path"])
+        # Search all project dirs
+        project_dirs = []
+        for folder in project_data["folders"]:
+            project_dirs.append(folder["path"])
 
-      # Initialize result buffer/view
-      self.result_buffer = resultbuffer.ResultBuffer(win, user_input)
+        # Initialize result buffer/view
+        self.result_buffer = resultbuffer.ResultBuffer(win, user_input)
 
-      # Start search thread
-      self.search_thread = filesearcher.FileSearcherThread(project_dirs, user_input, self.result_queue)
-      self.search_thread.start()
+        # Start search thread
+        self.search_thread = filesearcher.FileSearcherThread(project_dirs, user_input, self.result_queue)
+        self.search_thread.start()
 
-      # Come back in 1 ms to start handling results on an alternate thread
-      sublime.set_timeout_async(self.handle_search_results, 1)
+        # Come back in 1 ms to start handling results on an alternate thread
+        sublime.set_timeout_async(self.handle_search_results, 1)
 
-   def handle_search_results(self):
-      """
-      Handle search results that search thread places on the result queue
-      """
-      while self.search_thread.isAlive() or (self.result_queue.empty() == False):
-         self.update_status()
+    def handle_search_results(self):
+        """
+        Handle search results that search thread places on the result queue
+        """
+        while self.search_thread.isAlive() or (self.result_queue.empty() == False):
+            self.update_status()
 
-         # Check if result buffer has been closed - in this case we cancel the
-         # search.
-         if (self.search_cancelled == False) and (self.result_buffer.is_closed()):
-            self.search_thread.stop()
-            self.search_cancelled = True
+            # Check if result buffer has been closed - in this case we cancel the
+            # search.
+            if self.result_buffer.is_closed():
+                self.search_thread.stop()
+                self.search_cancelled = True
+                break
 
-         # If there are any results
-         if not self.result_queue.empty():
-            result = self.result_queue.get()
+            # If there are any results
+            if not self.result_queue.empty():
+                result = self.result_queue.get()
 
-            # Update number of hits but ensure it does not include warning/error strings
-            if len(result["result"]):
-               if 0 not in result["result"]:
-                  self.num_hits = self.num_hits + len(result["result"])
-                  self.num_file_hits = self.num_file_hits + 1
+                # Update number of hits but ensure it does not include warning/error strings
+                if len(result["result"]):
+                    if 0 not in result["result"]:
+                        self.num_hits = self.num_hits + len(result["result"])
+                        self.num_file_hits = self.num_file_hits + 1
 
-            # If we reach excessive limit start dropping results and stop search
-            if (self.excessive_hits_count != 0) and (self.num_hits > self.excessive_hits_count):
-               self.search_thread.stop()
-               self.search_cancelled = True
+                # If we reach excessive limit start dropping results and stop search thread
+                if (self.excessive_hits_count != 0) and (self.num_hits > self.excessive_hits_count):
+                    self.search_thread.stop()
+                    self.search_cancelled = True
+                    break
 
-            # Update result view
-            if not self.search_cancelled:
-               self.result_buffer.insert_result(result)
-            self.result_queue.task_done()
+                # Update result view
+                self.result_buffer.insert_result(result)
+                self.result_queue.task_done()
 
-      # We are done searching
-      self.set_final_status()
+        # We are done searching
+        self.set_final_status()
 
-   def update_status(self):
-      """
-      Update text in status bar
-      """
-      if time.time() > (self.last_status_update + 0.2):
-         self.last_status_update = time.time()
-         win = sublime.active_window()
-         self.status_string_idx = self.status_string_idx + 1
+    def update_status(self):
+        """
+        Update text in status bar
+        """
+        if time.time() > (self.last_status_update + 0.2):
+            self.last_status_update = time.time()
+            win = sublime.active_window()
+            self.status_string_idx = self.status_string_idx + 1
 
-         if self.status_string_idx == 0:
-            win.status_message("FindInProject: Searching in project | [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
-         elif self.status_string_idx == 1:
-            win.status_message("FindInProject: Searching in project / [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
-         elif self.status_string_idx == 2:
-            win.status_message("FindInProject: Searching in project - [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
-         elif self.status_string_idx > 2:
-            win.status_message("FindInProject: Searching in project \ [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
-            self.status_string_idx = 0
+            if self.status_string_idx == 0:
+                win.status_message("FindInProject: Searching in project | [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
+            elif self.status_string_idx == 1:
+                win.status_message("FindInProject: Searching in project / [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
+            elif self.status_string_idx == 2:
+                win.status_message("FindInProject: Searching in project - [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
+            elif self.status_string_idx > 2:
+                win.status_message("FindInProject: Searching in project \ [%i hits across %i files so far]" % (self.num_hits, self.num_file_hits))
+                self.status_string_idx = 0
 
-   def set_final_status(self):
-      """
-      Set final text in status bar
-      """
-      win = sublime.active_window()
-      if self.search_cancelled:
-         win.status_message("FindInProject: Search cancelled (due to close or excessive number of hits)")
-      else:
-         win.status_message("FindInProject: Search done [%i hits across %i files]" % (self.num_hits, self.num_file_hits))
+    def set_final_status(self):
+        """
+        Set final text in status bar
+        """
+        win = sublime.active_window()
+        if self.search_cancelled:
+            win.status_message("FindInProject: Search cancelled (due to close or excessive number of hits)")
+        else:
+            win.status_message("FindInProject: Search done [%i hits across %i files]" % (self.num_hits, self.num_file_hits))
--- filesearcher.py
+++ filesearcher.py
@@ -7,133 +7,132 @@ import sublime
 
 
 class FileSearcherThread(threading.Thread):
-   """
-   Search directories recursively in separate thread and push results onto a
-   queue.
-   """
-   def __init__(self, paths, target_string, result_queue):
-      super().__init__()
-      self._stop_thread = threading.Event()
-      self.paths = paths
-      self.target_string = target_string
-      self.result_queue = result_queue
+    """
+    Search directories recursively in separate thread and push results onto a
+    queue.
+    """
+    def __init__(self, paths, target_string, result_queue):
+        super().__init__()
+        self._stop_thread = threading.Event()
+        self.paths = paths
+        self.target_string = target_string
+        self.result_queue = result_queue
 
-      settings = sublime.load_settings('FindInProject.sublime-settings')
-      exts_to_ignore = settings.get('find_in_project_ignore_extensions', [])
-      self.exts_to_ignore = [x.lower() for x in exts_to_ignore]
-      dirs_to_ignore = settings.get('find_in_project_ignore_dirs', [])
-      self.dirs_to_ignore = [x.lower() for x in dirs_to_ignore]
-      self.encodings = settings.get('find_in_project_encodings', ["utf-8"])
-      self.skip_binary = settings.get('find_in_project_skip_binary_files', True)
-      self.max_file_size = settings.get('find_in_project_max_file_size_mb', 20)*1000000
-      self.follow_symlinks = settings.get('find_in_project_follow_sym_links', False)
-      self.max_line_len = settings.get('find_in_project_max_line_len', 100)
-      self.show_warning_on_open_fail = settings.get('find_in_project_show_warning_on_open_failure', False)
-      self.show_warning_size_skip = settings.get('find_in_project_show_warning_on_size_skip', False)
-      self.show_warning_binary_skip = settings.get('find_in_project_show_warning_on_binary_skip', False)
+        settings = sublime.load_settings('FindInProject.sublime-settings')
+        exts_to_ignore = settings.get('find_in_project_ignore_extensions', [])
+        self.exts_to_ignore = [x.lower() for x in exts_to_ignore]
+        dirs_to_ignore = settings.get('find_in_project_ignore_dirs', [])
+        self.dirs_to_ignore = [x.lower() for x in dirs_to_ignore]
+        self.encodings = settings.get('find_in_project_encodings', ["utf-8"])
+        self.skip_binary = settings.get('find_in_project_skip_binary_files', True)
+        self.max_file_size = settings.get('find_in_project_max_file_size_mb', 20)*1000000
+        self.follow_symlinks = settings.get('find_in_project_follow_sym_links', False)
+        self.max_line_len = settings.get('find_in_project_max_line_len', 100)
+        self.show_warning_on_open_fail = settings.get('find_in_project_show_warning_on_open_failure', False)
+        self.show_warning_size_skip = settings.get('find_in_project_show_warning_on_size_skip', False)
+        self.show_warning_binary_skip = settings.get('find_in_project_show_warning_on_binary_skip', False)
 
-   def stop(self):
-      """
-      Stop the thread after it is done searching the current file. The thread
-      waits until the result queue is empty, then it terminates.
-      """
-      self._stop_thread.set()
+    def stop(self):
+        """
+        Stop the thread after it is done searching the current file.
+        """
+        self._stop_thread.set()
 
-   def run(self):
-      """
-      Override the run method from threading.Thread to do a search when the
-      thread is started. This should not be called directly obviously.
-      """
-      for path in self.paths:
-         for root, dirs, files in os.walk(path, followlinks=self.follow_symlinks):
-            # Remove the excluded dirs in-place so os.walk() won't recurse into them
-            dirs[:] = [d for d in dirs if d.lower() not in self.dirs_to_ignore]
-            for file in files:
-               if self._stop_requested():
-                  return
+    def run(self):
+        """
+        Override the run method from threading.Thread to do a search when the
+        thread is started. This should not be called directly obviously.
+        """
+        for path in self.paths:
+            for root, dirs, files in os.walk(path, followlinks=self.follow_symlinks):
+                # Remove the excluded dirs in-place so os.walk() won't recurse into them
+                dirs[:] = [d for d in dirs if d.lower() not in self.dirs_to_ignore]
+                for file in files:
+                    if self._stop_requested():
+                        return
 
-               file_ext = os.path.splitext(file)[1][1:]
-               if file_ext.lower() in self.exts_to_ignore:
-                  continue
+                    file_ext = os.path.splitext(file)[1][1:]
+                    if file_ext.lower() in self.exts_to_ignore:
+                        continue
 
-               filepath = os.path.join(root, file)
-               file_size = os.path.getsize(filepath)
-               if file_size > self.max_file_size:
-                  result = collections.OrderedDict()
-                  if self.show_warning_size_skip:
-                     result[0] = "Skipped file due to size (%.2f MB)." % (file_size/1000000., )
-               else:
-                  result = self._search_file(filepath)
+                    filepath = os.path.join(root, file)
+                    file_size = os.path.getsize(filepath)
+                    if file_size > self.max_file_size:
+                        result = collections.OrderedDict()
+                        if self.show_warning_size_skip:
+                            result[0] = "Skipped file due to size (%.2f MB)." % (file_size/1000000., )
+                    else:
+                        result = self._search_file(filepath)
 
-               if len(result):
-                  ret = {"filepath": filepath, "result": result}
-                  self.result_queue.put(ret)
+                    if len(result):
+                        ret = {"filepath": filepath, "result": result}
+                        self.result_queue.put(ret)
 
-      # Wait until all results have been read from the queue - then terminate
-      self.result_queue.join()
+        # Wait until all results have been read from the queue - then terminate
+        self.result_queue.join()
 
-   def _stop_requested(self):
-      """
-      Check if stop has been requested.
-      """
-      flag = self._stop_thread.wait(0)
-      return flag
+    def _stop_requested(self):
+        """
+        Check if stop has been requested.
+        """
+        flag = self._stop_thread.wait(0)
+        return flag
 
-   def _search_file(self, path):
-      """
-      Search a file for the target file. Skip binaries if configured.
-      """
-      ret = collections.OrderedDict()
-      target_len = len(self.target_string)
-      for enc in self.encodings:
-         try:
-            with open(path, "r", encoding=enc) as target_file:
-               for line_num, line_content in enumerate(target_file, start=1):
-                  # Check for binary file
-                  if self.skip_binary and '\0' in line_content:
-                     ret = collections.OrderedDict()
-                     if self.show_warning_binary_skip:
-                        ret[0] = "Skipped binary file."
-                     return ret
+    def _search_file(self, path):
+        """
+        Search a file for the target string. Skip binaries if configured.
+        """
+        ret = collections.OrderedDict()
+        target_len = len(self.target_string)
+        for enc in self.encodings:
+            try:
+                with open(path, "r", encoding=enc) as target_file:
+                    for line_num, line_content in enumerate(target_file, start=1):
+                        # Check for binary file
+                        if self.skip_binary and '\0' in line_content:
+                            ret = collections.OrderedDict()
+                            if self.show_warning_binary_skip:
+                                ret[0] = "Skipped binary file."
+                            return ret
 
-                  # Search line for target string
-                  if self.target_string.lower() in line_content.lower():
-                     if len(line_content) > (self.max_line_len + target_len):
-                        line_content = self._limit_line(line_content)
-                     ret[line_num] = line_content
-         except:
-            # Probably using wrong encoding
-            continue
-         else:
-            return ret
+                        # Search line for target string
+                        if self.target_string.lower() in line_content.lower():
+                            if len(line_content) > (self.max_line_len + target_len):
+                                line_content = self._limit_line(line_content)
+                            ret[line_num] = line_content
+            except:
+                # Probably using wrong encoding
+                continue
+            else:
+                return ret
 
-      if self.show_warning_on_open_fail:
-         ret[0] = "Failed to open file. This could be due to unknown/unspecified encoding."
+        if self.show_warning_on_open_fail:
+            ret[0] = "Failed to open file. This could be due to unknown/unspecified encoding."
 
-      return ret
+        return ret
 
-   def _limit_line(self, line):
-      """
-      Limit the provided line according to the settings.
-      """
-      single_side_len = int(self.max_line_len/2)
-      loc = line.lower().find(self.target_string)
+    def _limit_line(self, line):
+        """
+        Limit the provided line according to the settings.
+        """
+        single_side_len = int(self.max_line_len/2)
+        loc = line.lower().find(self.target_string)
 
-      start = loc - single_side_len
-      if start < 0:
-         start = 0
+        start = loc - single_side_len
+        if start < 0:
+            start = 0
 
-      end = loc + len(self.target_string)
-      end = end + single_side_len
-      if (end >= len(line)):
-         end = len(line)-1
+        end = loc + len(self.target_string)
+        end = end + single_side_len
+        if (end >= len(line)):
+            end = len(line)-1
 
-      limited_line = ""
-      if start != 0:
-         limited_line = limited_line + "[…]"
+        limited_line = ""
+        if start != 0:
+            limited_line = limited_line + "[…]"
 
-      limited_line = limited_line + line[start:end]
-      if end != len(line)-1:
-         limited_line = limited_line + "[…]"
+        limited_line = limited_line + line[start:end]
+        if end != len(line)-1:
+            limited_line = limited_line + "[…]"
 
-      return limited_line
+        return limited_line
